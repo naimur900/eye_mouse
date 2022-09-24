83cfad3541b91758b63052b0aa57c914902a3efb
NaimurRahman <naimur.rahman900@gmail.com>
2022-09-24 14:41:04 +0600

    can detect more points

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import mediapipe as mp
 # import pyautogui as pau
 
 cap = cv.VideoCapture(0)
-# face_mesh and another face_mesh must be same
-face_mesh = mp.solutions.face_mesh.FaceMesh() 
-    # cv.imshow('img', flipped_img_frame)
-    # cv.waitKey(1)   
+
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks = True) 
+    
 
 while True:
     flag , img_frame = cap.read()
 
-    if flag == False:
-        break
+    # if flag == False:
+    #     break
 
     flipped_img_frame = cv.flip(img_frame, 1)
 
@@ -21,13 +20,19 @@ while True:
     rgb_img_frame = cv.cvtColor(flipped_img_frame, cv.COLOR_BGR2RGB)
 
     result = face_mesh.process(rgb_img_frame)
+    faces = result.multi_face_landmarks
 
-
-    for facial_landmarks in result.multi_face_landmarks:
-        for i in range(468):
-            pnt = facial_landmarks.landmark[i]
-            x = int(pnt.x * img_frame_width)
-            y = int(pnt.y * img_frame_height)
+    if faces == None:
+        break
+    
+    for face in faces:
+        # print(len(face.landmark))
+        
+        for i in range(478):
+            point = face.landmark[i]
+        
+            x = int(point.x * img_frame_width)
+            y = int(point.y * img_frame_height)
             
             cv.circle(flipped_img_frame, (x,y), 1, (0, 255, 0), -1)
 
