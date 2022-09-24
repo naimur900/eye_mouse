75337eb9f5c4bf1ea02e08a8b3fc7a1213834884
NaimurRahman <naimur.rahman900@gmail.com>
2022-09-24 12:53:42 +0600

    just detects

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 import cv2 as cv
 import mediapipe as mp
-import pyautogui as pau
+# import pyautogui as pau
 
-cam = cv.VideoCapture(0)
+cap = cv.VideoCapture(0)
 # face_mesh and another face_mesh must be same
 face_mesh = mp.solutions.face_mesh.FaceMesh() 
+    # cv.imshow('img', flipped_img_frame)
+    # cv.waitKey(1)   
 
 while True:
-    _ , frame = cam.read()
-    flipped_frame = cv.flip(frame, 1)
-    # cv.imshow('img', flipped_frame)
-    # cv.waitKey(1) 
-    frame_height, frame_width, _ = frame.shape
+    flag , img_frame = cap.read()
 
-    rgb_frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-    output = face_mesh.process(rgb_frame)
-    landmark_faces = output.multi_face_landmarks
-    # print(landmark_faces)
-    if landmark_faces:
-        landmark_points = landmark_faces[0].landmark
-        for landmark in landmark_points:
-            x = int(landmark.x * frame_width)
-            y = int(landmark.y * frame_height)
-            print(x,y)
-            color = (255, 133, 233)
-            cv.circle(frame, (x,y), 3, color)
+    if flag == False:
+        break
 
-    cv.imshow('img', flipped_frame)
+    flipped_img_frame = cv.flip(img_frame, 1)
+
+    img_frame_height, img_frame_width, channel = flipped_img_frame.shape
+
+    rgb_img_frame = cv.cvtColor(flipped_img_frame, cv.COLOR_BGR2RGB)
+
+    result = face_mesh.process(rgb_img_frame)
+
+
+    for facial_landmarks in result.multi_face_landmarks:
+        for i in range(468):
+            pnt = facial_landmarks.landmark[i]
+            x = int(pnt.x * img_frame_width)
+            y = int(pnt.y * img_frame_height)
+            
+            cv.circle(flipped_img_frame, (x,y), 1, (0, 255, 0), -1)
+
+    cv.imshow('img', flipped_img_frame)
     cv.waitKey(1) 
